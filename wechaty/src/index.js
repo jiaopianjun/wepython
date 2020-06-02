1ef2bfa21bdb0bd61a540de69b57c5464babc3a3
xin.cui <xin.cui@ly.com>
2020-06-02 11:37:40 +0800

    feat: 🎸 基于wechaty的机器人

--- wechaty/src/index.js
+++ wechaty/src/index.js
@@ -3,10 +3,10 @@ const { Wechaty } = require("wechaty") // Wechaty核心包
 const { PuppetPadplus } = require("wechaty-puppet-padplus") // padplus协议包
 const config = require("./config") // 配置文件
 
-const onScan = require("./onScan") // 机器人需要扫描二维码时监听回调
-const onRoomJoin = require("./onRoomJoin") // 加入房间监听回调
-const onMessage = require("./onMessage") // 消息监听回调
-const onFriendShip = require("./onFriendShip") // 好友添加监听回调
+const onScan = require("./scan") // 机器人需要扫描二维码时监听回调
+const onRoomJoin = require("./groupJoin") // 加入房间监听回调
+const onMessage = require("./message") // 消息监听回调
+const onFriendShip = require("./friend") // 好友添加监听回调
 
 // 初始化
 const bot = new Wechaty({
